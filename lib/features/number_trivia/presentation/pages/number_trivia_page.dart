8462238552c498d2ca6eaec3b324e8d0cdd165bc
diegopontes87 <diego.pontes87@gmail.com>
2022-10-12 17:05:08 -0300

    finished to construct the layout layer

---
+++ lib/features/number_trivia/presentation/pages/number_trivia_page.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/bloc/number_trivia_state.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/widgets/loading_widget.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/widgets/message_display.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/widgets/trivia_controls.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/widgets/trivia_display.dart';
+import '../../../../injection_container.dart';
+
+class NumberTriviaPage extends StatelessWidget {
+  const NumberTriviaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Number Trivia'),
+      ),
+      body: SingleChildScrollView(
+        child: buildBody(context),
+      ),
+    );
+  }
+
+  BlocProvider<NumberTriviaBloc> buildBody(BuildContext context) {
+    return BlocProvider(
+      create: (_) => getIt<NumberTriviaBloc>(),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 10),
+              // Top half
+              BlocBuilder<NumberTriviaBloc, NumberTriviaState>(
+                builder: (context, state) {
+                  if (state is EmptyState) {
+                    return const MessageDisplay(
+                      message: 'Start searching!',
+                    );
+                  } else if (state is LoadingState) {
+                    return const LoadingWidget();
+                  } else if (state is LoadedState) {
+                    return TriviaDisplay(numberTrivia: state.numberTrivia);
+                  } else if (state is ErrorState) {
+                    return MessageDisplay(
+                      message: state.errorMessage,
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+              const SizedBox(height: 20),
+              // Bottom half
+              const TriviaControls()
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/number_trivia/presentation/widgets/trivia_controls.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/bloc/number_trivia_bloc.dart';
+import 'package:tdd_clean_arch/features/number_trivia/presentation/bloc/number_trivia_event.dart';
+
+class TriviaControls extends StatefulWidget {
+  const TriviaControls({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  TriviaControlsState createState() => TriviaControlsState();
+}
+
+class TriviaControlsState extends State<TriviaControls> {
+  final controller = TextEditingController();
+  String? inputStr;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: controller,
+          keyboardType: TextInputType.number,
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Input a number',
+          ),
+          onChanged: (value) {
+            inputStr = value;
+          },
+          onSubmitted: (_) {
+            dispatchConcrete();
+          },
+        ),
+        const SizedBox(height: 10),
+        Row(
+          children: <Widget>[
+            Expanded(
+              child: ElevatedButton(
+                onPressed: dispatchConcrete,
+                child: const Text('Search'),
+              ),
+            ),
+            const SizedBox(width: 10),
+            Expanded(
+              child: ElevatedButton(
+                onPressed: dispatchRandom,
+                child: const Text('Get random trivia'),
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  void dispatchConcrete() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForConcreteNumberEvent(numberString: inputStr ?? ''));
+  }
+
+  void dispatchRandom() {
+    controller.clear();
+    BlocProvider.of<NumberTriviaBloc>(context).add(GetTriviaForRandomNumberEvent());
+  }
+}
